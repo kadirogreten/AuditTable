a92d6855f55f49d1466b5bbd46b95f2f797c088f
kadir öğreten <kadirogreten@hotmail.com>
2019-04-10 13:46:48 +0300

    Create a Web Application with identity and entityframework

---
+++ AuditTableWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AuditTableWebApp.MvcApplication" Language="C#" %>
---
+++ AuditTableWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AuditTableWebApp.Startup))]
+namespace AuditTableWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
